adcc3a02ada213cb24a18beb6b691f045c9e87a1
David Galache Soriano <davidgalache7@gmail.com>
2020-07-06 23:33:14 +0200

    ft_memchr finished

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dgalache <dgalache@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 12:20:06 by dgalache          #+#    #+#             */
-/*   Updated: 2020/07/03 18:08:51 by dgalache         ###   ########.fr       */
+/*   Updated: 2020/07/06 23:32:07 by dgalache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	while (n--)
-		if (*((unsigned char *) s++) == (unsigned char)c)
+	{
+		if (*((unsigned char *) s) == (unsigned char)c)
 			return((void *)s--);
+		s++;
+	}
 	return (NULL);
 }
