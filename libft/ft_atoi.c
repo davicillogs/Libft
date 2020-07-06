cbdd1ab943705bd933b83ee3e0f479e4817fd69e
David Galache Soriano <davidgalache7@gmail.com>
2020-07-06 23:19:10 +0200

    atoi arreglada

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dgalache <dgalache@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 12:45:20 by dgalache          #+#    #+#             */
-/*   Updated: 2020/07/03 15:09:33 by dgalache         ###   ########.fr       */
+/*   Updated: 2020/07/06 23:18:05 by dgalache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	ft_atoi(const char *str)
 
 	total = 0;
 	sign = 1;
-	while (ft_isspace(*str++) != 0);
+	while (ft_isspace(*str))
+		str++;
 	if (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
